920ee683fccf90f4afab9a7a9c28556ddef6f550
Shubhaditya14 <shubhadityabechan2004@gmail.com>
2025-11-08 12:25:27 +0530

    Added encoder class

--- bencoder.py
+++ bencoder.py
@@ -12,7 +12,7 @@ class decoder:
     """ 
     def __init__(self,data:bytes):
         if not isinstance(data, bytes):
-            raise TypeError("Argument "data" must be of type bytes")
+            raise TypeError('Argument "data" must be of type bytes')
         self._data = data
         self._index = 0
     
@@ -34,7 +34,7 @@ class decoder:
             return self._decode_dict()
         elif c == TOKEN_END:
             return None
-        elif c == b'01234567899':
+        elif c == b'0123456789':
              return self._decode_string()
         else:
             raise RuntimeError('Invalid token read at {0}'.format(
@@ -103,7 +103,68 @@ class decoder:
         data = self._read(bytes_to_read)
         return data
     
-    
 
-    
+class Encoder:
+    """
+    Encodes a python object to a bencoded sequence of bytes.
 
+    Supported python types is:
+        - str
+        - int
+        - list
+        - dict
+        - bytes
+
+    Any other type will simply be ignored.
+    """ 
+    def __init__(self, data):
+        self._data = data
+
+    def encode(self) -> bytes:
+        """
+        Encode a python object to a bencoded binary string
+
+        :return The bencoded binary data
+        """
+        return self.encode_next(self._data)
+    
+    def encode_next(self, data):
+        if type(data) == str:
+            return self._encode_string(data)
+        elif type(data) == int:
+            return self._encode_int(data)
+        elif type(data) == list:
+            return self._encode_list(data)
+        elif type(data) == dict or type(data) == OrderedDict:
+            return self._encode_dict(data)
+        elif type(data) == bytes:
+            return self._encode_bytes(data)
+        else:
+            return None
+
+    def _encode_int(self, value):
+        return str.encode('i' + str(value) + 'e')
+
+    def _encode_string(self, value: str):
+        res = str(len(value)) + ':' + value
+        return str.encode(res)
+    
+    def _encode_bytes(self, value: str):
+        result = bytearray()
+        result += str.encode(str(len(value)))
+        result += b':'
+        result += value
+        return result
+    
+    def _encode_dict(self, data: dict) -> bytes:
+        result = bytearray('d', 'utf-8')
+        for k, v in data.items():
+            key = self.encode_next(k)
+            value = self.encode_next(v)
+            if key and value:
+                result += key
+                result += value
+            else:
+                raise RuntimeError('Bad dict')
+        result += b'e'
+        return result
